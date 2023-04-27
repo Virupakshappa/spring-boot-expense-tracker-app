59c19704eedd004249c34c2f238acaab1bb66c4d
Virupakshappa7 <lnu.vir@northeastern.edu>
2023-04-27 00:37:13 -0400

    SBETP-1: Initial commit

---
+++ src/main/java/com/janadri/springbootexpensetrackerapp/SpringBootExpenseTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.janadri.springbootexpensetrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExpenseTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExpenseTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janadri/springbootexpensetrackerapp/SpringBootExpenseTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janadri.springbootexpensetrackerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExpenseTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
