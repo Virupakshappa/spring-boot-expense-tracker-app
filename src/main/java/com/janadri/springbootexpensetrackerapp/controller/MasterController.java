52ea301e04be5bdf1dff0ab844ea5b217433c543
Virupakshappa7 <lnu.vir@northeastern.edu>
2023-05-07 12:42:28 -0400

    SBETP-1: Updating the repo with jsp file

---
+++ src/main/java/com/janadri/springbootexpensetrackerapp/controller/MasterController.java
@@ -0,0 +1,15 @@
+package com.janadri.springbootexpensetrackerapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MasterController {
+
+	@RequestMapping("/")
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView("Home");
+		return mv;
+	}
+}
